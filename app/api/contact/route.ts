560237a3f6f6d81f260adab17284ac00c176c7fa
Shreyas <shreyassalyan917@gmail.com>
2024-12-12 23:31:55 +0530

    initial commit

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -29,7 +29,72 @@ export async function POST(request: Request) {
       to: process.env.NEXT_PUBLIC_EMAIL_USER, // Your email where the contact form is sent
       replyTo: email, // This is the user's email for the reply
       subject: `New Contact Form Submission from ${name}`,
-      text: `Name: ${name}\nEmail: ${email}\nCompany: ${company}\nMessage: ${message}`,
+      html: `
+        <html>
+          <head>
+            <style>
+              body {
+                font-family: Arial, sans-serif;
+                color: #333;
+                line-height: 1.5;
+              }
+              .container {
+                width: 100%;
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 20px;
+                border: 1px solid #ddd;
+                border-radius: 8px;
+                background-color: #f9f9f9;
+              }
+              h2 {
+                color: #0056b3;
+                text-align: center;
+              }
+              .content {
+                padding: 15px;
+                background-color: #fff;
+                border: 1px solid #ddd;
+                border-radius: 8px;
+              }
+              .field {
+                margin-bottom: 10px;
+              }
+              .field span {
+                font-weight: bold;
+              }
+              .footer {
+                margin-top: 20px;
+                text-align: center;
+                font-size: 0.9em;
+                color: #777;
+              }
+            </style>
+          </head>
+          <body>
+            <div class="container">
+              <h2>New Contact Form Submission</h2>
+              <div class="content">
+                <div class="field">
+                  <span>Name:</span> ${name}
+                </div>
+                <div class="field">
+                  <span>Email:</span> ${email}
+                </div>
+                <div class="field">
+                  <span>Company:</span> ${company}
+                </div>
+                <div class="field">
+                  <span>Message:</span><br />${message}
+                </div>
+              </div>
+              <div class="footer">
+                <p>This email was generated automatically. Please do not reply to this email.</p>
+              </div>
+            </div>
+          </body>
+        </html>
+      `,
     };
     
 
